bba5393801b659306d87b74257952c5f52c2f95a
Momchil Marinov <mmarinov@spbtv.com>
2014-11-11 15:49:45 +0100

    added SSL certificate and server converted to use HTTPS

--- config/config.js
+++ config/config.js
@@ -1,18 +1,22 @@
 /**
  * Created by mmarinov on 09-Nov-14.
  */
-var path = require('path');
+var path = require('path'),
+    fs = require('fs');
+
 var rootPath = path.normalize(__dirname + '../../');
 
 module.exports = {
     development: {
         rootPath: rootPath,
         db: 'mongodb://@localhost/supn',
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        certificate: fs.readFileSync(__dirname + '/development.pfx')
     },
     production: {
         rootPath: rootPath,
         db: 'mongodb://@localhost/supn',
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        certificate: null
     }
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-
+var https = require('https');
 var express = require('express');
+
 var env = process.env.NODE_ENV || 'development';
 var config = require('./config/config')[env];
 var app = express();
@@ -9,7 +10,12 @@ require('./config/express')(app);
 require('./config/routes')(app);
 require('./config/mongoose')(config);
 
-app.listen(config.port, function () {
+var options = {
+    pfx: config.certificate,
+    passphrase: '123456'
+};
+
+https.createServer(options, app).listen(config.port, function () {
     console.log('Server listening on port ' + config.port);
     console.log('Server Environment: ' + env);
 });

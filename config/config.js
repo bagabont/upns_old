f486649fafebcde4d3bb4ad90cace3c4fae3fbf8
Momchil Marinov <mmarinov@spbtv.com>
2014-11-17 12:45:56 +0100

    config updateD

--- config/config.js
+++ config/config.js
@@ -13,9 +13,9 @@ module.exports = {
     },
     production: {
         rootPath: rootPath,
-        db: ' mongodb://admin:admin@ds055680.mongolab.com:55680/upns',
+        db: 'mongodb://admin:admin@ds055680.mongolab.com:55680/upns',
         port: process.env.PORT,
         certificate: fs.readFileSync(__dirname + '/cert_dev.pfx'),
-        certificatePassphrase: ''
+        certificatePassphrase: '123456'
     }
 };
